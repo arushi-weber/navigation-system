0e8918758c2dd363bc3f71df94937d055918e630
anubhutikala <anubhutikala2006@gmail.com>
2025-11-28 21:35:43 +0530

    Enhance 5location.h with field comments

--- 5location.h
+++ 5location.h
@@ -2,18 +2,19 @@
 #define LOCATION_H
 
 struct AdjNode {
-    char name[30];
-    int distance;
-    struct AdjNode *next;
+    char name[30];            // connected place
+    int distance;             // distance in meters
+    struct AdjNode *next;     // next connection
 };
 
 struct Location {
-    char name[30];
-    struct AdjNode *connections;
-    struct Location *next;
+    char name[30];            // location name
+    struct AdjNode *connections;  
+    struct Location *next;    // next location in list
 };
 
-struct Location* buildMap();
+struct Location* buildMap();  
 void printMap(struct Location *head);
+void addLocationNamed(struct Location **head, const char *newName,const char *connectName, int dist);
 
 #endif
